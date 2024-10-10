f409b0c5414c64e49c7a723d2d5efb88ccd66d3a
Jotaniya-Darshit <jotaniyadarshit1235@gmail.com>
2024-10-10 16:35:03 +0530

    React Basics

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -1,37 +1,35 @@
-import { Component, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export default class ClassCom extends Component {
-  constructor(props) {
-    super();
-    this.state = { c : 0}
-  }
-  increment = () => {
-    this.setState({c: this.state.c + 1})
-  }
-  decrement = () => {
-    this.setState({c: this.state.c - 1})
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
-  componentDidMount(){
-    console.log("Darshit");
-  }
-
-  componentDidUpdate()
-  {
-    console.log("Updating");
-  }
-
-  render(){
-    return (
+  return (
+    <>
       <div>
-        <h1>Counter Using State</h1>
-        <h1>{this.state.c}</h1>
-        <button onClick={this.increment}>Add</button>
-        <button onClick={this.decrement}>Del</button>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    )
-  }
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
